e465eb4ffc48d0aab4b30ff5cdb41dab1de99bbb
s2sprint <69420623+s2sprint@users.noreply.github.com>
2020-09-13 22:14:30 +0900

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,23 @@
+input.onButtonPressed(Button.A, function () {
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(349, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Double))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(349, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Double))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(349, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Double))
+    music.playTone(330, music.beat(BeatFraction.Double))
+    music.playTone(262, music.beat(BeatFraction.Double))
+    music.playTone(330, music.beat(BeatFraction.Double))
+    music.playTone(294, music.beat(BeatFraction.Double))
+})
+basic.forever(function () {
+	
+})
